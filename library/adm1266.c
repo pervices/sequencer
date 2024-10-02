dc2b4a77126a58a8202355099bc3bf7229906864
Isaac Ho <tsz.h@pervices.com>
2024-10-02 21:10:57 +0000

    SEQ: Not stopping the program if I2C read failed

--- library/adm1266.c
+++ library/adm1266.c
@@ -19,8 +19,7 @@ void ADM1266_Print_Current_State(__u8 ADM1266_NUM, __u8 *ADM1266_Address, __u8 *
 	for (__u8 i = 0; i < ADM1266_NUM; i++)
 	{
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 1, ADM1266_datain) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_Current_State, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_Current_State\n");
 		}
 		temp = ADM1266_datain[0] + (ADM1266_datain[1] * 256)-1;
 		printf("ADM1266 at Address %#02x is in '", ADM1266_Address[i]);
@@ -38,8 +37,7 @@ void ADM1266_Get_Current_State(__u8 ADM1266_NUM, __u8 *ADM1266_Address, __u8 *AD
 	for (__u8 i = 0; i < ADM1266_NUM; i++)
 	{
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 1, ADM1266_datain) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_Current_State, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_Current_State\n");
 		}
 		ADM1266_Current_State[i] = ADM1266_datain[0] + (ADM1266_datain[1]*256);
 	}
@@ -177,8 +175,7 @@ __u8 ADM1266_Get_Part_Locked(__u8 ADM1266_NUM, __u8 *ADM1266_Address, __u8 *ADM1
 	for (__u8 i = 0; i < ADM1266_NUM; i++)
 	{
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 1, ADM1266_datain) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_Part_Locked, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_Part_Locked\n");
 		}
 		ADM1266_Part_Locked[i] = (ADM1266_datain[0]&4)>>2 ;
 		temp = temp | ADM1266_Part_Locked[i];
@@ -195,8 +192,7 @@ __u8 ADM1266_Get_Sys_CRC(__u8 ADM1266_NUM, __u8 *ADM1266_Address)
 	for (__u8 i = 0; i < ADM1266_NUM; i++)
 	{
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 1, ADM1266_datain) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_Sys_CRC, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_Sys_CRC\n");
 		}
 		if (ADM1266_datain[0] > 31)
 		{
@@ -215,8 +211,7 @@ void ADM1266_Print_MFR_ID(__u8 ADM1266_NUM, __u8 *ADM1266_Address)
 	{
 		printf("MFR_ID for ADM1266 at Address %#02x is : ", ADM1266_Address[i]);
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 33, ADM1266_datain) == 0){
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_MFR_ID, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_MFR_ID\n");
 		}
 		for (__u8 j = 1; j < 33; j++)
 		{
@@ -235,8 +230,7 @@ void ADM1266_Print_MFR_MODEL(__u8 ADM1266_NUM, __u8 *ADM1266_Address)
 	{
 		printf("MFR_MODEL for ADM1266 at Address %#02x is : ", ADM1266_Address[i]);
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 33, ADM1266_datain) == 0){
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_MFR_MODEL, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_MFR_MODEL\n");
 		}
 		for (__u8 j = 1; j < 33; j++)
 		{
@@ -255,8 +249,7 @@ void ADM1266_Print_MFR_REVISION(__u8 ADM1266_NUM, __u8 *ADM1266_Address)
 	{
 		printf("MFR_REVISION for ADM1266 at Address %#02x is : ", ADM1266_Address[i]);
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 9, ADM1266_datain) == 0){
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_MFR_REVISION, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_MFR_REVISION\n");
 		}
 		for (__u8 j = 1; j < 9; j++)
 		{
@@ -275,8 +268,7 @@ void ADM1266_Print_MFR_LOCATION(__u8 ADM1266_NUM, __u8 *ADM1266_Address)
 	{
 		printf("MFR_LOCATION for ADM1266 at Address %#02x is : ", ADM1266_Address[i]);
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 49, ADM1266_datain) == 0){
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_MFR_LOCATION, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_MFR_LOCATION\n");
 		}
 		for (__u8 j = 1; j < 49; j++)
 		{
@@ -295,8 +287,7 @@ void ADM1266_Print_MFR_DATE(__u8 ADM1266_NUM, __u8 *ADM1266_Address)
 	{
 		printf("MFR_DATE for ADM1266 at Address %#02x is : ", ADM1266_Address[i]);
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 17, ADM1266_datain) == 0){
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_MFR_DATE, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_MFR_DATE\n");
 		}
 		for (__u8 j = 1; j < 17; j++)
 		{
@@ -315,8 +306,7 @@ void ADM1266_Print_MFR_SERIAL(__u8 ADM1266_NUM, __u8 *ADM1266_Address)
 	{
 		printf("MFR_SERIAL for ADM1266 at Address %#02x is : ", ADM1266_Address[i]);
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 33, ADM1266_datain) == 0){
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_MFR_SERIAL, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_MFR_SERIAL\n");
 		}
 		for (__u8 j = 1; j < 33; j++)
 		{
@@ -336,8 +326,7 @@ void ADM1266_Print_User_Data(__u8 ADM1266_NUM, __u8 *ADM1266_Address)
 	{
 		printf("User_Data for ADM1266 at Address %#02x is : ", ADM1266_Address[i]);
 		if (i2c_block_write_block_read(ADM1266_Address[i], 5, dataout,253, ADM1266_datain) == 0){
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_User_Data, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Print_User_Data\n");
 		}
 		temp = ADM1266_datain[1] + (ADM1266_datain[2] * 256);
 		for (__u8 j = 3; j < temp; j++)
@@ -356,8 +345,7 @@ void ADM1266_Get_IC_Device_ID(__u8 ADM1266_NUM, __u8 *ADM1266_Address, __u8 *ADM
 	for (__u8 i = 0; i < ADM1266_NUM; i++)
 	{
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 4, ADM1266_datain) == 0){
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_IC_Device_ID, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_IC_Device_ID\n");
 		}
 		for (__u8 j = 1; j < 4; j++)
 		{
@@ -376,8 +364,7 @@ void ADM1266_Get_IC_Device_Rev(__u8 ADM1266_NUM, __u8 *ADM1266_Address, __u8 *AD
 	{
 		mode = 0;
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 9, ADM1266_datain) == 0){
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_IC_Device_Rev, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_IC_Device_Rev\n");
 		}
 		for (__u8 j = 4; j < 7; j++)
 		{
@@ -414,8 +401,7 @@ void ADM1266_Get_Refresh_Counter(__u8 ADM1266_NUM, __u8 *ADM1266_Address, __u16 
 	for (__u8 i = 0; i < ADM1266_NUM; i++)
 	{
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 9, ADM1266_datain) == 0){
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_Refresh_Counter, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_Refresh_Counter\n");
 		}
 		ADM1266_Refresh_Counter[i] = ADM1266_datain[3] + (ADM1266_datain[4] * 256);
 	}
@@ -438,8 +424,7 @@ void ADM1266_Get_CRC_Error_Counter(__u8 ADM1266_NUM, __u8 *ADM1266_Address, __u1
 	for (__u8 i = 0; i < ADM1266_NUM; i++)
 	{
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 9, ADM1266_datain) == 0){
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_CRC_Error_Counter, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_CRC_Error_Counter\n");
 		}
 		ADM1266_CRC_Error_Counter[i] = ADM1266_datain[5] + (ADM1266_datain[6] * 256);
 	}
@@ -575,8 +560,7 @@ void ADM1266_Get_All_Data(__u8 ADM1266_NUM, __u8 *ADM1266_Address, __u8 *ADM1266
 		k = 1;
 		dataout[0] = 0xE8;
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 52, ADM1266_datain) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_All_Data 1, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_All_Data 1\n");
 		}
 		for (j = 1; j < 5; j++) 
 		{
@@ -597,8 +581,7 @@ void ADM1266_Get_All_Data(__u8 ADM1266_NUM, __u8 *ADM1266_Address, __u8 *ADM1266
 		k = 1;
 		dataout[0] = 0xE7;
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 18, ADM1266_datain) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_All_Data 2, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_All_Data 2\n");
 		}
 		for (j = 1; j < 5; j++)
 		{
@@ -618,14 +601,12 @@ void ADM1266_Get_All_Data(__u8 ADM1266_NUM, __u8 *ADM1266_Address, __u8 *ADM1266
 		}
 		dataout[0] = 0xE9;
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 3, ADM1266_datain) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_All_Data 3, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_All_Data 3\n");
 		}
 		temp = (ADM1266_datain[2] * 256) + ADM1266_datain[1];
 		dataout[0] = 0xEA;
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 3, ADM1266_datain) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_All_Data 4, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_All_Data 4\n");
 		}
 		temp2 = (ADM1266_datain[2] * 256) + ADM1266_datain[1];
 		temp2 = ADM1266_GPIO_Map(temp2);
@@ -879,8 +860,7 @@ void ADM1266_System_Read(__u8 ADM1266_Num, __u8 *ADM1266_Address, __u8 *ADM1266_
 	sdPtr = 0;
 	__u8 dataout[5] = { 0xD7, 0x03, 0x80, 0x00, 0x00 };
 	if (i2c_block_write_block_read(ADM1266_Address[0], 5, dataout, 128, ADM1266_datain) == 0) {
-		printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_System_Read 1, check hardware connection. Program stopped.\n");
-		exit(EXIT_FAILURE);
+		printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_System_Read 1\n");
 	}
 
 	for (m = 0; m < ADM1266_datain[29] + 1; m++) {
@@ -898,8 +878,7 @@ void ADM1266_System_Read(__u8 ADM1266_Num, __u8 *ADM1266_Address, __u8 *ADM1266_
 		n = 0;
 		__u8 dataout[5] = { 0xD7, 0x03, 0x03, 0x00, 0x00 };
 		if (i2c_block_write_block_read(ADM1266_Address[i], 5, dataout, 3, ADM1266_datain) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_System_Read 2, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_System_Read 2\n");
 		}
 		Data_Length = ADM1266_datain[1] + (ADM1266_datain[2] * 256);
 		j = 128;
@@ -920,8 +899,7 @@ void ADM1266_System_Read(__u8 ADM1266_Num, __u8 *ADM1266_Address, __u8 *ADM1266_
 
 
 			if (i2c_block_write_block_read(ADM1266_Address[i], 5, dataout, n + 1, ADM1266_datain) == 0) {
-				printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_System_Read 3, check hardware connection. Program stopped.\n");
-				exit(EXIT_FAILURE);
+				printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_System_Read 3\n");
 			}
 			if (k == 0 && l == 128 && n == 128)
 			{
@@ -1225,8 +1203,7 @@ void ADM1266_Get_Num_Records(__u8 *ADM1266_Address, __u16 *ADM1266_Record_Index,
 	__u8 ADM1266_datain[5];
 	__u8 dataout[1] = { 0xE6 };
 	if (i2c_block_write_block_read(ADM1266_Address[0], 1, dataout, 5, ADM1266_datain) == 0) {
-		printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_Num_Records, check hardware connection. Program stopped.\n");
-		exit(EXIT_FAILURE);
+		printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_Num_Records\n");
 	}
 	*ADM1266_Record_Index = ADM1266_datain[3];
 	*ADM1266_Num_Records = ADM1266_datain[4];
@@ -1248,8 +1225,7 @@ void ADM1266_Get_BB_Raw_Data(__u8 ADM1266_Num, __u8 *ADM1266_Address, __u8 index
 	{
 		dataout[2] = temp;
 		if (i2c_block_write_block_read(ADM1266_Address[i], 3, dataout, 64, ADM1266_datain) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_BB_Raw_Data, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_BB_Raw_Data\n");
 		}
 		for (m = 0; m < 64; m++) {
 			ADM1266_BB_Data[n21(0, i, m, ADM1266_Num, 64)] = ADM1266_datain[m];
@@ -1662,8 +1638,7 @@ void ADM1266_FW_Boot_Rev(__u8 ADM1266_Address, __u8 *ADM1266_datain)
     __u8 dataout[1];
     dataout[0] = 0xAE;
     if (i2c_block_write_block_read(ADM1266_Address, 0x01, dataout, 9, ADM1266_datain) == 0) {
-		printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_FW_Boot_Rev, check hardware connection. Program stopped.\n");
-		exit(EXIT_FAILURE);
+		printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_FW_Boot_Rev\n");
 	}  
 }
 
@@ -1760,14 +1735,12 @@ void ADM1266_Margin_Single_Percent(__u8 ADM1266_Address, __u8 ADM1266_Pin, float
 	//Read back exp and ment
 	dataout[0] = 0x20;
 	if (i2c_block_write_block_read(ADM1266_Address, 1, dataout, 1, ADM1266_datain) == 0) {
-		printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Margin_Single_Percent 1, check hardware connection. Program stopped.\n");
-		exit(EXIT_FAILURE);
+		printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Margin_Single_Percent 1\n");
 	}
 	__u8 exp = ADM1266_datain[0];
 	dataout[0] = 0x21;
 	if (i2c_block_write_block_read(ADM1266_Address, 1, dataout, 2, ADM1266_datain) == 0) {
-		printf("\033[0;31m[ERROR]\033[0m I2C read failed ADM1266_Margin_Single_Percent 2, check hardware connection. Program stopped.\n");
-		exit(EXIT_FAILURE);
+		printf("\033[0;31m[ERROR]\033[0m I2C read failed ADM1266_Margin_Single_Percent 2\n");
 	}
 
 	//Calculate nominal Value
@@ -1935,8 +1908,7 @@ void ADM1266_Margin_Single(__u8 ADM1266_Address, char *ADM1266_Pin_Name, __u8 AD
         {
             dataout[2] = dac_index;
             if (i2c_block_write_block_read(ADM1266_Address, 3, dataout, 3, ADM1266_datain) == 0) {
-				printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Margin_Single, check hardware connection. Program stopped.\n");
-				exit(EXIT_FAILURE);
+				printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Margin_Single\n");
 			}
 
             ADM1266_DAC_Mapping = ADM1266_datain[1] + (ADM1266_datain[2] << 8);
@@ -2002,8 +1974,7 @@ void ADM1266_DAC_Mapping(__u8 *ADM1266_Address, __u8 ADM1266_NUM, struct ADM1266
         {
             dataout[2] = dac_counter;
             if (i2c_block_write_block_read(ADM1266_Address[device_counter], 3, dataout, 3, ADM1266_datain) == 0) {
-				printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_DAC_Mapping, check hardware connection. Program stopped.\n");
-				exit(EXIT_FAILURE);
+				printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_DAC_Mapping\n");
 			}
             ADM1266_DAC_Mapping = ADM1266_datain[1] + (ADM1266_datain[2] << 8);         
             ADM1266_DAC_data[(dac_counter+counter_multi)].input_channel = (ADM1266_DAC_Mapping >> 6) & 0x1F;
@@ -2055,8 +2026,7 @@ __u8 ADM1266_Refresh_Status(__u8 *ADM1266_Address, __u8 ADM1266_NUM)
     for (__u8 loop = 0; loop < ADM1266_NUM; loop++)
     {
         if (i2c_block_write_block_read(ADM1266_Address[loop], 0x01, dataout, 1, ADM1266_datain) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Refresh_Status, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Refresh_Status\n");
 		}
         ADM1266_datain[0] = (ADM1266_datain[0] & 0x08) >> 3;
 
@@ -2137,8 +2107,7 @@ __u8 ADM1266_DAC_Config(__u8 ADM1266_Address, __u8 ADM1266_DAC_Number)
         data_buffer[2] = ADM1266_DAC_Number;
  
         if (i2c_block_write_block_read(ADM1266_Address, 0x03, data_buffer, 0x03, data_buffer) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_DAC_Config, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_DAC_Config\n");
 		}
         margin_mode = data_buffer[1] & 0x03;
  
@@ -2399,8 +2368,7 @@ __u16 ADM1266_All_CRC_Status(__u8 ADM1266_Address)
     dataout[0] = 0xED;
     __u8 ADM1266_datain[2];
     if (i2c_block_write_block_read(ADM1266_Address, 0x01, dataout, 0x02, ADM1266_datain) == 0) {
-		printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_All_CRC_Status, check hardware connection. Program stopped.\n");
-		exit(EXIT_FAILURE);
+		printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_All_CRC_Status\n");
 	}
     return ((ADM1266_datain[0] + (ADM1266_datain[1] << 8)) >> 4);
 }
@@ -2415,8 +2383,7 @@ __u8 ADM1266_Device_Present(__u8 *ADM1266_Address, __u8 ADM1266_NUM)
     for (__u8 loop = 0; loop < ADM1266_NUM; loop++)
     {        
         if (i2c_block_write_block_read(ADM1266_Address[loop], 0x01, dataout, 0x04, ADM1266_datain) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Device_Present, check hardware connection. Program stopped.\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Device_Present\n");
 		}
         if (((ADM1266_datain[1] == 0x42) || (ADM1266_datain[1] == 0x41)) && (ADM1266_datain[2] == 0x12) && (ADM1266_datain[2] = 0x66))
         {
@@ -2439,8 +2406,7 @@ __u8 ADM1266_Get_Part_Locked_System(__u8 ADM1266_NUM, __u8 *ADM1266_Address)
 	for (__u8 i = 0; i < ADM1266_NUM; i++)
 	{
 		if (i2c_block_write_block_read(ADM1266_Address[i], 1, dataout, 1, ADM1266_datain) == 0) {
-			printf("\033[0;31m[ERROR]\033[0m \033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_Part_Locked_System, check hardware connection. Program stopped., check hardware connection\n");
-			exit(EXIT_FAILURE);
+			printf("\033[0;31m[ERROR]\033[0m \033[0;31m[ERROR]\033[0m I2C read failed at ADM1266_Get_Part_Locked_System\n");
 		}		
 		temp = temp | ((ADM1266_datain[0] & 4) >> 2);
 	}
